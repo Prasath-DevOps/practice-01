550d83f606a784fa5d37f28ef446d4234ca939e2
Pragan S <spragan2308@gmail.com>
2026-09-12 20:58:10 +0530

    practice

--- script.js
+++
@@ -1,9 +0,0 @@
-let count = 0;
-
-const button = document.getElementById("count-button");
-const display = document.getElementById("count-display");
-
-button.addEventListener("click", function () {
-  count++;
-  display.textContent = count;
-});
